505088db5284b59019217c0cddda64a47d7f17a7
mrt39 <mertoktar39@gmail.com>
2023-11-07 14:48:32 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>HERE IS THE MAINPAGE</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
