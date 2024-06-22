56c5b8bd4b29daa61b7a50e569ac98d5893ab56a
mrt39 <mertoktar39@gmail.com>
2024-06-22 19:40:14 +0300

    Store session in mongodb to fix the disconnection problem

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,16 @@ require('dotenv').config()
 const express = require("express");
 const bodyParser = require("body-parser");
 
-
 //hashing, cookies 
 const session = require('express-session');
 const {passport} = require( "./passport.js")
 
 
-
 //cors
 const cors = require("cors");
 
-
 //require other js files
 const authRoute = require("./routes/routes.js");
-
 
 const app = express();
 app.use(
@@ -27,7 +23,6 @@ app.use(
   })
 );
 
-
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -35,20 +30,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use('/images', express.static('images'))
 
 
+
+
+app.set('trust proxy', 1); //add this for production
+
+//storing session in mongodb because storing it in client is unstable for production
+//(works fine for development but starts to randomly log people out in production, when deployed to server)
+const MongoDBStore = require('connect-mongodb-session')(session);
+const dev_db_url = "mongodb://127.0.0.1:27017/chatChirpUserDB"
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URI || dev_db_url,
+  collection: 'mySessions'
+});
+
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: false,
+    saveUninitialized: true,
+    proxy: true, //add this for production
+    store: store, //store to mongodb
     cookie: {
-      secure: false, // Set to true in production if served over HTTPS
-      sameSite: 'strict' // Optional, set based on your requirements
+      secure: true, // Set to true in production if served over HTTPS, "false" for development
+      sameSite: 'none' // "none" for production, "strict" for development
   }
 }));
 
-
 app.use(passport.initialize());
 app.use(passport.session());
-
 
 
 
@@ -58,10 +66,10 @@ app.use(passport.session());
 app.use("/", authRoute);
 
 
-
 app.listen("5000", () => {
   console.log("Server is running!");
 });
+
 
 
 
